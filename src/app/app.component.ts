5a44aa077aa5d479beb346a1059374c958026aae
JamesYanik <janikjakub2005@proton.me>
2024-04-08 11:00:02 +0200

    changeJSON btn support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reactiveForm';
+  json = {"ala" : {"type":"text"},"ma": {"type":"number"},"kota":{"type":"select","options":["a","b","c"]}}
+  turned = false
+
+  changeJSON(){
+    if (this.turned){
+      this.json = {"ala" : {"type":"text"},"ma": {"type":"number"},"kota":{"type":"select","options":["a","b","c"]}}
+    } else {
+      this.json = {"kota":{"type":"select","options":["a","b","c"]},"ma": {"type":"text"},"ala" : {"type":"number"}}
+    }
+    this.turned = !this.turned
+  }
 }
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,52 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+
+interface Control {
+  name: string,
+  type: string,
+  options?: string[]
+}
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgFor, NgIf],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+
+export class FormComponent {
+  @Input() json!: any;
+  public form: FormGroup = this.fb.group({});
+  
+  controls: Control[] = []
+
+  ngOnChanges() {
+    this.controls = []
+    this.createForm()
+  }
+
+
+  createForm(){
+    for (let key of Object.keys(this.json)){
+      if(this.json[key]['type'] == 'select'){
+        this.controls.push({name: key, type: this.json[key]['type'], options: this.json[key]['options']})
+      } else{
+        this.controls.push({name: key, type: this.json[key]['type']})
+      }
+      this.form.addControl(
+        key,
+        this.fb.control('', [])
+      );
+    }
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  onSubmit(){
+    console.log('Form valid: ', this.form.valid);
+    console.log('Form values: ', this.form.value);
+  }
+}
